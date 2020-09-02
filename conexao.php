a84cf18770c8eae1574c993453f451f78f99fdd3
juliomarques56 <40442660+juliomarques56@users.noreply.github.com>
2020-09-02 17:16:43 -0400

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php 
-    $servername = "localhost";
-    $database = "curso_estoque";
-    $username = "root";
-    $password = "";
+    $servername = "z8dl7f9kwf2g82re.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+    $database = "wws2ttakwsfiuzvn";
+    $username = "cj7jxj7noqlz7lq7";
+    $password = "duy0ft7iosasmu0g";
 
     $conexao = mysqli_connect($servername,  $username, $password, $database);
 ?>
